beffe97e1e025b4baf0c09b604705dbb1fe2cfa7
steffan christensen <stefc5949@gmail.com>
2019-07-15 15:55:40 +0200

    Update MovementScript(add cap to velocity)

--- traingame/Assets/Scripts/MovementScript.cs
+++ traingame/Assets/Scripts/MovementScript.cs
@@ -6,6 +6,7 @@ public class MovementScript : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
     public float speed;
+    public float maxSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,8 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (rigidbody.velocity.x>maxSpeed|| rigidbody.velocity.x<-maxSpeed) {capSpeed();}
+
         if (Input.GetKey(KeyCode.D))
             {rigidbody.AddForce(new Vector2(speed, 0)*Time.deltaTime,ForceMode2D.Force);
             //gameObject.transform.Translate(new Vector3(speed, 0,0) * Time.deltaTime);
@@ -25,5 +28,13 @@ public class MovementScript : MonoBehaviour
             //gameObject.transform.Translate(new Vector3(-speed, 0,0) * Time.deltaTime);
             }
 
+        print(rigidbody.velocity);
+    }
+
+    void capSpeed() {
+        if (rigidbody.velocity.x > maxSpeed) { rigidbody.velocity = new Vector2(maxSpeed, 0); }
+        else if (rigidbody.velocity.x < -maxSpeed) {rigidbody.velocity = new Vector2(-maxSpeed,0); }
+        
+        
     }
 }
