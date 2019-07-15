b3730a406d43d6ca2678d65172f0893f70b6e2b5
steffan christensen <stefc5949@gmail.com>
2019-07-15 13:12:20 +0200

    Add assets folder

---
+++ traingame/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent(Rigidbody2D);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if() 
+    }
+}
