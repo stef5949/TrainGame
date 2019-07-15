f8742607b13f446773b5e77897e048b8e0edfd06
Chris <christobagger@gmail.com>
2019-07-15 13:07:46 +0200

    First Upload, basic windows and gamemanager

---
+++ Train Horror Game/Assets/Scripts/windowScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windowScript : MonoBehaviour
+{
+    public int maxSound;
+
+    private bool playerHere = false;
+    private int currentSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSound = 0;
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerHere = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerHere = false;   
+    }
+
+    public void monsterTrigger()
+    {
+        if(playerHere)
+        {
+            playerDeath();
+        }
+
+    }
+
+    public void setSound(int soundIncrease) {
+        int x;
+        x = soundIncrease;
+        currentSound += x;
+    }
+
+    public void getSound();
+    private void playerDeath()
+    {
+
+
+    }
+
+}
+
+
+
